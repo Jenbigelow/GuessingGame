a7d71736dcff9de702401dfed9683e76e375cce3
Jenbigelow <jennifer.o.bigelow@gmail.com>
2024-03-28 22:57:04 -0500

    End of the night

--- game.py
+++ game.py
@@ -2,6 +2,22 @@
 import random
 
 name = input("Hello player - what's your name?")
-number = randint(1,100)
+number = random.randint(1,100)
+
+guess = 0
+tries = 0
 
 print(number)
+
+while number != guess:
+    guess = int(input("Guess a number between 1 and 100: "))
+    tries += 1
+    if guess > number:
+        print("Guess is too high. Try again.")
+    elif guess < number:
+        print("Guess is too low. Try again.")
+    else:
+        break
+
+
+print(f"Your guess is correct! It took you {tries} attempts.")
