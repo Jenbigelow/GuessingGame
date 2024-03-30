453f7767967664d2e55a997d295463d0b839938d
Jenbigelow <jennifer.o.bigelow@gmail.com>
2024-03-30 17:03:58 -0500

    Repeat comparison

--- game.py
+++ game.py
@@ -1,23 +1,51 @@
 """A number-guessing game."""
 import random
 
-name = input("Hello player - what's your name?")
-number = random.randint(1,100)
+def guessing_game():
+    name = input("Hello player - what's your name?")
+    number = random.randint(1,100)
 
-guess = 0
-tries = 0
-
-print(number)
-
-while number != guess:
-    guess = int(input("Guess a number between 1 and 100: "))
-    tries += 1
-    if guess > number:
-        print("Guess is too high. Try again.")
-    elif guess < number:
-        print("Guess is too low. Try again.")
-    else:
-        break
+    guess = 0
+    tries = 0
 
 
-print(f"Your guess is correct! It took you {tries} attempts.")
+    while number != guess:
+        tries += 1
+        try:
+            guess = int(input("Guess a number between 1 and 100: "))
+            if guess < 1 or guess > 100:
+                print("Guess needs to be between 1 and 100.")
+            elif guess > number:
+                print("Guess is too high. Try again.")
+            elif guess < number:
+                print("Guess is too low. Try again.")
+            else:
+                break
+        except:
+            print("You did not enter a valid integer between 1 and 100.")
+
+
+    print(f"Your guess is correct! It took you {tries} attempts.")
+    return tries
+
+
+def play_the_game_again():
+    comparing_attempts = []
+    while True:
+       tries = guessing_game()
+       comparing_attempts.append(tries)
+       answer = input("Do you want to play again? Y/N  ").lower()
+       if answer == "n":
+            break
+       
+
+    print(min(comparing_attempts))
+    return min(comparing_attempts)
+
+
+
+play_the_game_again()
+
+
+
+         
